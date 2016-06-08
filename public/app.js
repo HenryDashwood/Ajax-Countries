f2efb7c55a27ec10e59cb369c28ce1e38b159877
Henry Dashwood <hcndashwood@gmail.com>
2016-06-08 09:56:22 +0100

    Coded along with Keith

--- public/app.js
+++ public/app.js
@@ -1,17 +1,12 @@
 window.onload = function() {
   var url = 'https://restcountries.eu/rest/v1';
   var request = new XMLHttpRequest();
-
-  // console.log(request);
   request.open("GET", url);
   request.onload = function() {
     if(request.status === 200) {
-      console.log("got the data");
       var jsonString = request.responseText;
       var countries = JSON.parse(jsonString);
       var country = countries[0];
-      console.log(country.name);
-      console.log(country);
       main(countries);
     }
   }
@@ -19,27 +14,42 @@ window.onload = function() {
 
   var main = function(countries) {
 
-    countries.forEach(function(country) {
-      appendCountry(country);
-    })
-
-    select.onchange = function(countries) {
-      var select = document.getElementById('country-list');
-      for(country of countries) {
-        if(country.name === select.value) {
-          console.log(country.name);
-          console.log(country.capital);
-          console.log(country.population);
-        }
-      }
+    populateSelect(countries);
+    var selected = countries[0];
+    var cached = localStorage.getItem("selectedCountry");
+    
+    if(cached) {
+      selected = JSON.parse(cached);
+      document.querySelector("#countries").selectedIndex =selected.index;
     }
+
+    updateDisplay(selected);
+    document.querySelector("#info").style.display = "block";
   }
 
-  var appendCountry = function(country) {
-    var option = document.createElement('option');
-    option.innerText = country.name;
-    var select = document.getElementById('country-list');
-    select.appendChild(option);
+  var populateSelect = function(countries) {
+    var parent = document.querySelector("#countries");
+    countries.forEach(function(item, index) {
+      item.index = index;
+      var option = document.createElement("option");
+      option.value = index;
+      option.text = item.name;
+      parent.appendChild(option);
+    });
+    parent.style.display = "block";
+    parent.addEventListener("change", function() {
+      var index = this.value;
+      var country = countries[index];
+      updateDisplay(country);
+      localStorage.setItem("selectedCountry", JSON.stringify(country));
+    })
+  }
+
+  var updateDisplay = function(country) {
+    var tags =document.querySelectorAll("#info p");
+    tags[0].innerText = country.name;
+    tags[1].innerText = country.capital;
+    tags[2].innerText = country.population;
   }
 
 }
